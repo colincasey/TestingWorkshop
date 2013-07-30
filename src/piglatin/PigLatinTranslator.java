096894f6b4e71ce0e8a5455b933048e07cdb8422
Colin Casey <casey.colin@gmail.com>
2013-07-30 19:01:58 -0300

    pig latin example solution

--- src/piglatin/PigLatinTranslator.java
+++ src/piglatin/PigLatinTranslator.java
@@ -1,4 +1,55 @@
 package piglatin;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class PigLatinTranslator {
+    private final Pattern startingVowels = Pattern.compile("^[aeiou]+", Pattern.CASE_INSENSITIVE);
+    private final Pattern regularVowelsWithY = Pattern.compile("[aeiouy]", Pattern.CASE_INSENSITIVE);
+    private final Pattern regularVowels = Pattern.compile("[aeiou]", Pattern.CASE_INSENSITIVE);
+    private final Pattern capitalized = Pattern.compile("^[A-Z]");
+
+    public String translate(String word) {
+        boolean isCapitalized = isCapitalized(word);
+        String translatedWord = startsWithConsonants(word) ?
+            doConsonantTranslation(word) :
+            doVowelTranslation(word);
+        translatedWord = adjustForCapitalization(translatedWord, isCapitalized);
+        return translatedWord;
+    }
+
+    private String adjustForCapitalization(String translatedWord, boolean capitalized) {
+        String adjustedWord = translatedWord.toLowerCase();
+        if(capitalized) {
+            adjustedWord = adjustedWord.substring(0, 1).toUpperCase() + adjustedWord.substring(1);
+        }
+        return adjustedWord;
+    }
+
+    private boolean isCapitalized(String word) {
+        return capitalized.matcher(word).find();
+    }
+
+    private String doConsonantTranslation(String word) {
+        Pattern pattern = startsWithY(word) ?
+            regularVowels :
+            regularVowelsWithY;
+        Matcher matcher = pattern.matcher(word);
+        matcher.find();
+        return word.substring(matcher.start()) +
+                word.substring(0, matcher.start()) +
+                "ay";
+    }
+
+    private boolean startsWithY(String word) {
+        return word.startsWith("y") || word.startsWith("Y");
+    }
+
+    private String doVowelTranslation(String word) {
+        return word + "way";
+    }
+
+    private boolean startsWithConsonants(String word) {
+        return !startingVowels.matcher(word).find();
+    }
 }
--- test/piglatin/PigLatinTranslatorTest.java
+++ test/piglatin/PigLatinTranslatorTest.java
@@ -1,5 +1,11 @@
 package piglatin;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 /*
 
 PIG LATIN RULES
@@ -35,4 +41,40 @@ PIG LATIN RULES
 
  */
 public class PigLatinTranslatorTest {
+    private PigLatinTranslator pigLatinTranslator;
+
+    @Before
+    public void setUp() throws Exception {
+        pigLatinTranslator = new PigLatinTranslator();
+    }
+
+    @Test
+    public void should_translate_words_that_begin_with_consonants() throws Exception {
+        assertThat(translate("happy"), is("appyhay"));
+        assertThat(translate("duck"), is("uckday"));
+        assertThat(translate("glove"), is("oveglay"));
+    }
+
+    @Test
+    public void should_translate_words_that_begin_with_vowels() throws Exception {
+        assertThat(translate("egg"), is("eggway"));
+        assertThat(translate("inbox"), is("inboxway"));
+        assertThat(translate("eight"), is("eightway"));
+    }
+
+    @Test
+    public void should_differentiate_between_y_as_a_vowel_and_y_as_a_consonant() throws Exception {
+        assertThat(translate("yellow"), is("ellowyay"));
+        assertThat(translate("style"), is("ylestay"));
+    }
+
+    @Test
+    public void should_retain_capilization() throws Exception {
+        assertThat(translate("Radian"), is("Adianray"));
+
+    }
+
+    private String translate(String word) {
+        return pigLatinTranslator.translate(word);
+    }
 }
